77e0124f7b51545eca5a6c107dd856beac1ecc0f
小若憲史 <kenjikowaka@kowakakenjinoMacBook-Pro.local>
2023-05-27 02:37:05 +0900

    マイグレーション変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_25_051159) do
+ActiveRecord::Schema.define(version: 2023_05_25_032253) do
 
   create_table "posts", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2023_05_25_051159) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "reservations", force: :cascade do |t|
@@ -50,10 +51,11 @@ ActiveRecord::Schema.define(version: 2023_05_25_051159) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "name"
-    t.string "profile"
+    t.text "profile"
+    t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
